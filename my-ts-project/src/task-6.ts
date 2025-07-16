788b6d91be2efe394fff64839bc35f47f7710504
Oleksandr <netesa.sasha@gmail.com>
2025-07-16 21:54:34 +0300

    delete main

--- my-ts-project/src/task-6.ts
+++ my-ts-project/src/task-6.ts
@@ -4,8 +4,6 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-const num = getFirstElement<number>([1, 2, 3]); // 1
-const str = getFirstElement<string>(["a", "b", "c"]); // "a"
-const bln = getFirstElement<boolean>([true, false, true]); // true
-
-console.log(num, str, bln);
+console.log(getFirstElement<number>([1, 2, 3])); // 1
+console.log(getFirstElement<string>(["a", "b", "c"])); // "a"
+console.log(getFirstElement<boolean>([true, false, true])); // true
